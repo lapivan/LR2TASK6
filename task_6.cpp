7910d4b3b6ab48136dacdf034b36abf40508038e
ivan <lapunoujohn@gmail.com>
2024-09-16 22:52:08 +0300

    var8

--- task_6.cpp
+++ task_6.cpp
@@ -2,13 +2,11 @@
 #include <cmath>
 
 int main() {
-    double a, c, b, x, k, d, y, p, h;
+    double a, c, b, x, k, d, y, p;
     std::cout << "Enter x: " << std::endl;
     std::cin >> x;
     std::cout << "Enter p: " << std::endl;
     std::cin >> p;
-    std::cout << "Enter h: " << std::endl;
-    std::cin >> h;
     std::cout << "Enter k: " << std::endl;
     std::cin >> k;
     std::cout << "Enter k: " << std::endl;
@@ -17,9 +15,9 @@ int main() {
     std::cin >> c;
     std::cout << "Enter d: " << std::endl;
     std::cin >> d;
-    a = x - p;
-    b = log(h);
-    pow(0,78 * b + (pow(a, 3) / (k * c * d)));
+    a = log10(x);
+    b = x + exp(d);
+    y = (a + b) - (pow(c, 2) / k);
     std::cout << "Result: " << y;
     return 0;
 }
