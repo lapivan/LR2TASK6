acbbb87383c8a98972a6d0436012537f45654bf9
ivan <lapunoujohn@gmail.com>
2024-09-16 21:53:11 +0300

    var3

--- task_6.cpp
+++ task_6.cpp
@@ -2,20 +2,20 @@
 #include <cmath>
 
 int main() {
-    double a, x, p, b, k, d, c, y;
+    double n, x, p, b, k, d, c, q;
     std::cout << "Enter x: " << std::endl;
     std::cin >> x;
-    std::cout << "Enter d: " << std::endl;
-    std::cin >> d;
-    std::cout << "Enter c: " << std::endl;
-    std::cin >> c;
-    std::cout << "Enter k: " << std::endl;
-    std::cin >> k;
     std::cout << "Enter p: " << std::endl;
     std::cin >> p;
-    a = x+sin(p);
-    b = exp(k);
-y = 1 + (pow(k, 2)/2 * a * b) - b + d * c;
-std::cout << "Result: " << y;
+    std::cout << "Enter n: " << std::endl;
+    std::cin >> n;
+    std::cout << "Enter k: " << std::endl;
+    std::cin >> k;
+    std::cout << "Enter d: " << std::endl;
+    std::cin >> d;
+    b = cos(x);
+    c = p - n;
+q = (pow(b, 2)/ (k * d) + b * pow(c, 3));
+std::cout << "Result: " << q;
     return 0;
 }
